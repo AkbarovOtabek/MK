eda90f0b30511fbda6cd4a36fe36c22bb3e2c640
AkbarovOtabek <60823128+AkbarovOtabek@users.noreply.github.com>
2021-10-11 20:54:04 +0500

    Add files via upload

---
+++ MK/main.js
@@ -0,0 +1,44 @@
+//alert('Fight...');
+//console.log('Fight...');
+ 
+let firstName='Scorpion';
+firstName='Sonya';
+const lastName='Blade';
+const count=4;
+
+console.log(firstName);
+console.log(lastName);
+console.log(typeof firstName);
+console.log(typeof count);
+
+const infinity=5/0;
+const notNumber=10*'as';
+console.log(infinity);
+console.log(typeof infinity);
+console.log(notNumber)
+console.log(typeof notNumber);
+
+const isBoolean=false;
+console.log(isBoolean);
+
+const symbol =Symbol();
+console.log(typeof symbol);
+
+const bigint=BigInt(3);
+console.log(typeof bigint);
+
+let someVariable;
+console.log(typeof someVariable);
+
+let someNull=null;
+console.log(typeof someNull);
+
+const obj={}
+console.log(typeof obj);
+
+const arr=[];
+const fn=function(){};
+console.log(typeof arr);
+console.log(typeof fn);
+
+
